afc32012f08b925a23ec75233375ebda9d08e293
sdi1600098 <tasos298@gmail.com>
2023-03-06 20:05:29 +0200

    coursity3

--- coursity3.py
+++ coursity3.py
@@ -1,4 +1,8 @@
-
+#List construction and list slicing
+#mylist: list of y elements, elements are a multiplication of x* (list index + 1)
+#mylist01: list of y elements that are either 1 if element of mylist with the same index is odd, 0 if it is even
+#mylist_a: list with the elements of mylist that are in odd numbered index
+#mylist_p: list with the elements of mylist that are in even numbered index
 
 x = int(input('x = '))
 y = int(input('y = '))
